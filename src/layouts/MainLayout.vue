e2a275a6c5f253c868d8278f533dc9e27c09089f
jinjin <737195059@qq.com>
2020-10-28 11:58:16 +0800

    初始代码

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,260 @@
+<template>
+  <q-layout view="hHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+          v-if="!$q.screen.gt.xs"
+        />
+        <q-avatar class="q-logo">
+          <img src="../assets/logo.svg" />
+        </q-avatar>
+        <q-toolbar-title
+          shrink
+          class="text-bold logo-text-primary"
+        >
+          管理系统
+          <span
+            class="q-ml-xs"
+            style="letter-spacing: 0.1em;font-size:12px;font-weight:500;"
+          >Quasar v1.14.1</span>
+        </q-toolbar-title>
+
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container class="main-page-container">
+
+      <q-drawer class="main-page-sidebar full-height"
+        v-model="leftDrawerOpen"
+        show-if-above
+        :mini="miniCheck"
+        @mouseover="miniState = false"
+        @mouseout="miniState = true"
+        bordered
+        mini-to-overlay
+        content-class="bg-grey-1"
+      >
+        <div class="sidebar-body">
+          <q-scroll-area class="fit">
+            <q-list>
+              <EssentialLink
+                v-for="link in essentialLinks"
+                :key="link.title"
+                v-bind="link"
+              />
+            </q-list>
+            <q-list>
+              <EssentialLink
+                v-for="link in essentialLinks"
+                :key="link.title"
+                v-bind="link"
+              />
+            </q-list>
+          </q-scroll-area>
+        </div>
+
+        <div
+          class="sidebar-footer row items-center"
+        >
+          <q-btn
+            flat
+            dense
+            round
+            @click="leftDrawerMini = !leftDrawerMini"
+            :icon="`${leftDrawerMini?'format_indent_increase':'format_indent_decrease'}`"
+            aria-label="Menu"
+            color="primary"
+            size="sm"
+          />
+        </div>
+
+      </q-drawer>
+
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import EssentialLink from 'components/EssentialLink.vue'
+
+const linksData = [
+  {
+    title: 'Docs23',
+    caption: 'quasar.dev',
+    icon: 'school',
+    link: 'https://quasar.dev'
+  },
+  {
+    title: 'Github',
+    caption: 'github.com/quasarframework',
+    icon: 'code',
+    link: 'https://github.com/quasarframework'
+  },
+  {
+    title: 'Discord Chat Channel',
+    caption: 'chat.quasar.dev',
+    icon: 'chat',
+    link: 'https://chat.quasar.dev'
+  },
+  {
+    title: 'Forum',
+    caption: 'forum.quasar.dev',
+    icon: 'record_voice_over',
+    link: 'https://forum.quasar.dev'
+  },
+  {
+    title: 'Twitter',
+    caption: '@quasarframework',
+    icon: 'rss_feed',
+    link: 'https://twitter.quasar.dev'
+  },
+  {
+    title: 'Facebook',
+    caption: '@QuasarFramework',
+    icon: 'public',
+    link: 'https://facebook.quasar.dev'
+  },
+  {
+    title: 'Quasar Awesome',
+    caption: 'Community Quasar projects',
+    icon: 'favorite',
+    link: 'https://awesome.quasar.dev'
+  },
+  {
+    title: 'Quasar Awesome',
+    caption: 'Community Quasar projects',
+    icon: 'favorite',
+    link: 'https://awesome.quasar.dev'
+  },
+  {
+    title: 'Quasar Awesome',
+    caption: 'Community Quasar projects',
+    icon: 'favorite',
+    link: 'https://awesome.quasar.dev'
+  },
+  {
+    title: 'Quasar Awesome',
+    caption: 'Community Quasar projects',
+    icon: 'favorite',
+    link: 'https://awesome.quasar.dev'
+  },
+  {
+    title: 'Quasar Awesome',
+    caption: 'Community Quasar projects',
+    icon: 'favorite',
+    link: 'https://awesome.quasar.dev'
+  },
+  {
+    title: 'Quasar Awesome',
+    caption: 'Community Quasar projects',
+    icon: 'favorite',
+    link: 'https://awesome.quasar.dev'
+  },
+  {
+    title: 'Quasar Awesome',
+    caption: 'Community Quasar projects',
+    icon: 'favorite',
+    link: 'https://awesome.quasar.dev'
+  },
+  {
+    title: 'Quasar Awesome',
+    caption: 'Community Quasar projects',
+    icon: 'favorite',
+    link: 'https://awesome.quasar.dev'
+  },
+  {
+    title: 'Quasar Awesome',
+    caption: 'Community Quasar projects',
+    icon: 'favorite',
+    link: 'https://awesome.quasar.dev'
+  },
+  {
+    title: 'Quasar Awesome',
+    caption: 'Community Quasar projects',
+    icon: 'favorite',
+    link: 'https://awesome.quasar.dev'
+  },
+  {
+    title: 'Quasar Awesome',
+    caption: 'Community Quasar projects',
+    icon: 'favorite',
+    link: 'https://awesome.quasar.dev'
+  },
+  {
+    title: 'Quasar Awesome',
+    caption: 'Community Quasar projects',
+    icon: 'favorite',
+    link: 'https://awesome.quasar.dev'
+  },
+  {
+    title: 'Quasar Awesome',
+    caption: 'Community Quasar projects',
+    icon: 'favorite',
+    link: 'https://awesome.quasar.dev'
+  }
+]
+
+export default {
+  name: 'MainLayout',
+  components: { EssentialLink },
+  data () {
+    return {
+      miniState: true,
+      leftDrawerMini: true,
+      leftDrawerOpen: false,
+      essentialLinks: linksData
+    }
+  },
+  computed: {
+    miniCheck: function () {
+      if (this.leftDrawerMini) {
+        return this.miniState
+      } else {
+        return false
+      }
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+.main-page-container
+  position: absolute
+  left: 0
+  top: 50px
+  right: 0
+  bottom: 0
+  overflow: hidden
+  padding-top: 0px !important
+.main-page-sidebar
+  position: absolute
+  border-right: 1px solid #eaebec
+  background-color: #fff
+  & .sidebar-body
+    position: absolute
+    top: 0
+    left: 0
+    right: 0
+    bottom: 45px
+  & .sidebar-footer
+    position: absolute
+    left: 0
+    right: 0
+    bottom: 0
+    height: 45px
+    padding-left: 13.5px
+.main-page-body
+  position: absolute
+  top: 0
+  right: 0
+  bottom: 0
+  overflow-y: auto
+  overflow-x: hidden
+</style>
